aa6ff6a8e72579bc313c3a22c75d6adfb3fe6339
Michael Donaghy <md401@srcf.ucam.org>
2012-10-11 20:17:18 +0100

    Copy the twisted basic example

--- httpcrawler.py
+++ httpcrawler.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import sys
+from twisted.internet import reactor
+from twisted.web.client import Agent
+from twisted.web.http_headers import Headers
+
+results = {} #global, for now
+agent = Agent(reactor)
 
 if('__main__' == __name__):
-    domain = sys.argv[1]
+    initialurl = sys.argv[1]
+    initialrequest = agent.request('GET', initialurl)
+    def cbResponse(ignored):
+        print('Response received')
+    initialrequest.addCallback(cbResponse)
+    def cbShutdown(ignored):
+        reactor.stop()
+    initialrequest.addBoth(cbShutdown) 
+    reactor.run()
+    print(results)
