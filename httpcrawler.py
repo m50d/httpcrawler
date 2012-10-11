498897a9564e067505f4df08e13fe8ca7dd75f2b
Michael Donaghy <md401@srcf.ucam.org>
2012-10-11 21:56:27 +0100

    Something like some actual crawling

--- httpcrawler.py
+++ httpcrawler.py
@@ -7,6 +7,9 @@ from twisted.web.http_headers import Headers
 from twisted.internet.protocol import Protocol
 from bs4 import BeautifulSoup
 from twisted.internet.ssl import ClientContextFactory
+from urlparse import urlparse
+
+def host(url): return urlparse(url).hostname
 
 outstandingrequests = []
 
@@ -21,10 +24,12 @@ class PageBodyParser(Protocol):
     def dataReceived(self, data):
         self.buffer += data
     def connectionLost(self, reason):
-        global outstandingrequests
+        global outstandingrequests, hostname
         # Using beautifulsoup, so we'll still try and parse a page even if we got an error
         soup = BeautifulSoup(self.buffer)
-        print(soup.prettify())
+        results[self.url] = True
+        newUrls = [link.get('href') for link in soup.find_all('a')]
+        [makeRequest(url) for url in newUrls if hostname == host(url) and (not url in outstandingrequests) and (not url in results) and (not urlparse(url).params)]
         outstandingrequests.remove(self.url)
         if(not outstandingrequests): reactor.stop() 
         else: print(outstandingrequests)
@@ -45,6 +50,8 @@ def makeRequest(url):
     return request
 
 if('__main__' == __name__):
-    initialrequest = makeRequest(sys.argv[1])
+    global hostname
+    hostname = host(sys.argv[1])
+    makeRequest(sys.argv[1])
     reactor.run()
     print(results)
